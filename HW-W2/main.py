e6599f81881a349ded5751cdb25efeb647555576
Sittichok Hongthong <sittichok.ho@gmail.com>
2021-01-29 22:20:46 +0700

    .

---
+++ HW-W2/main.py
@@ -0,0 +1,15 @@
+
+
+class Player:
+
+    def __init__(self,name):
+        self.name = name
+    
+    def Taketurn(self):
+
+        return "Taketurn"
+
+
+p = Player("Tong")
+print(p.name)
+#print(p.Taketurn())
